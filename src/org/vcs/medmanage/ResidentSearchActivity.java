82282df63a29013e920f52c927ac7fd652b4efd4
Pulkit Bhuwalka <pulkit.bosco05@gmail.com>
2013-10-11 07:02:10 -0400

    Initial layout for resident search added. Adding the dependency libs in so, we can push it to maven later if we get to it.

---
+++ src/org/vcs/medmanage/ResidentSearchActivity.java
@@ -0,0 +1,10 @@
+package org.vcs.medmanage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResidentSearchActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
