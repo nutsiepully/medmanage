d1c9d719f6ca20a87010dd915698b28d86a9e7b8
Samantha Tan <samanthawltan@gmail.com>
2013-10-23 10:08:02 -0400

    Merge branch 'master' of https://github.com/nutsiepully/medmanage Readding files again?
    Conflicts:
    	src/org/vcs/medmanage/ResidentMedicineActivity.java

---
+++ src/org/vcs/medmanage/ResidentMedicineActivity.java
@@ -0,0 +1,140 @@
+package org.vcs.medmanage;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+
+import db.DatabaseHelper;
+
+import entities.Resident;
+
+
+/* This class will be the Resident's profile page with a tab for the medications the residents are on
+ * 
+ * */
+public class ResidentMedicineActivity extends Activity {
+
+    private DatabaseHelper database;
+  
+    Resident currentResident;
+    
+    private ArrayAdapter<Resident> residentAdapter;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(UI_MODE_SERVICE, "Entered App");
+        setContentView(R.layout.resident_medicine);
+        // Set the default patient
+
+        /*RuntimeExceptionDao<Resident, Integer> dao = 
+        		((DatabaseHelper) getHelper()).getResidentDataDao();
+        
+        */
+        
+        displayPatientProfile(currentResident);
+        displayMedicineList(currentResident);
+        
+        //Changes on click :)
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	setTxtViews();
+            }
+        });
+        
+        }
+    
+    // Show the Patient Information in a normal fashion
+    public void displayPatientProfile(Resident res){
+    	
+    	// Change the patient profile picture
+    	//ImageView iv = (ImageView) findViewById(R.id.patientPicture);
+    	//iv.setImageBitmap(bitmap);
+    	Log.d(UI_MODE_SERVICE, "Entered displayPatientProfile");
+    	
+    }
+    
+    // List view of the information 
+    public void displayMedicineList(Resident res){
+    	
+    }
+    
+    public int updateTextView(String toThis, String name) {
+    	String finalString = "";
+    	TextView t;
+     	
+    	if(name.equals("txtPatientName")){
+    		t = (TextView) this.findViewById(R.id.txtPatientName);
+    		
+    		
+    	}   	
+     	else if(name.equals("txtPatientGender")){
+    		t = (TextView) this.findViewById(R.id.txtPatientGender);
+    		finalString = "Gender: ";
+    	}
+    	else if(name.equals("txtPatientRoom")){
+    		t = (TextView) this.findViewById(R.id.txtPatientRoom);
+    		finalString = "Room ";
+    	}
+    	else if(name.equals("txtPatientDiagnosis")){
+    		t = (TextView) this.findViewById(R.id.txtPatientDiagnosis);
+    		finalString = "Diagnosis: ";
+    	}
+    	else if(name.equals("txtPatientAge")){
+    		t = (TextView) findViewById(R.id.txtPatientAge);
+    		finalString = "Age: ";
+    	}
+    	else if(name.equals("txtPatientWeight")){
+    		t = (TextView) findViewById(R.id.txtPatientWeight);
+    		finalString = "Weight: ";
+    	}
+    	else if(name.equals("txtPatientRecentActions")){
+    		t = (TextView) findViewById(R.id.txtPatientRecentActions);
+    		finalString = "Recent Activity: \n";
+    	}
+    	else if(name.equals("txtPatientNotes")){
+    		t = (TextView) findViewById(R.id.txtPatientNotes);
+    		finalString = "Nurse Notes: \n";
+    	}
+    	else{
+    		// Didn't find any text view
+    		return -1;
+    	}
+    	
+    	finalString = finalString + toThis;
+    	Log.d(UI_MODE_SERVICE, "This is: " + finalString);
+       
+    	t.setText(finalString);
+        return 1;
+    }
+    
+    public void setTxtViews(){
+    	updateTextView("Phil Simms", "txtPatientName");
+    	updateTextView("Male", "txtPatientGender");
+    	updateTextView("24", "txtPatientAge");
+    	updateTextView("13", "txtPatientRoom");
+    	updateTextView("Melanoma", "txtPatientDiagnosis");
+    	updateTextView("165", "txtPatientWeight");
+    	updateTextView("10am: Gave tylenol for headache.", "txtPatientRecentActions");
+    	updateTextView("Beach", "txtPatientNotes");
+    }
+
+}
