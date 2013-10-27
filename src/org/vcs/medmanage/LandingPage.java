87e54d50ea8240421a231613feeb20e6c390c0c5
kurt-traver <kurt.traver@gmail.com>
2013-10-26 21:32:48 -0400

    Recent residents now show up on the landing page.

--- src/org/vcs/medmanage/LandingPage.java
+++ src/org/vcs/medmanage/LandingPage.java
@@ -12,6 +12,9 @@ import entities.RecentResidentUtils;
 import entities.Resident;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
 
 /**
  * This is the first page to be started when the application starts. It displays
@@ -40,7 +43,11 @@ public class LandingPage extends FragmentActivity {
 		residentDao =
 				getHelper().getResidentDataDao();
 		
+		//Get recent residents, if any, and add to view
+		List<Resident> recentResidents = getRecentResidents();
+		addRecentResidentsToView(recentResidents);
 		
+		//TODO: Calendar stuff here probably
 	}
 	
 	/**
@@ -53,6 +60,88 @@ public class LandingPage extends FragmentActivity {
 		recentResidents = recentUtils.getRecentResidents(residentDao);
 		
 		return recentResidents;
+	}
+	
+	/**
+	 * Adds the given recentResidents to the view.
+	 */
+	public void addRecentResidentsToView(List<Resident> recentResidents){
+		//Make all the fragments invisible by defualt
+		View recent1 = findViewById(R.id.recent_resident_1);
+		View recent2 = findViewById(R.id.recent_resident_2);
+		View recent3 = findViewById(R.id.recent_resident_3);
+		View recent4 = findViewById(R.id.recent_resident_4);
+		View recent5 = findViewById(R.id.recent_resident_5);
+		recent1.setVisibility(View.INVISIBLE);
+		recent2.setVisibility(View.INVISIBLE);
+		recent3.setVisibility(View.INVISIBLE);
+		recent4.setVisibility(View.INVISIBLE);
+		recent5.setVisibility(View.INVISIBLE);
+		
+		for(int i = 0; i < recentResidents.size(); i++){
+			Bundle residentArgs = new Bundle();
+			residentArgs.putString("ResidentName", recentResidents.get(i).getName());
+			residentArgs.putInt("RoomNumber", recentResidents.get(i).getRoomNumber());
+			switch(i){
+				case 0: {
+					ResidentFragment residentPreview = new ResidentFragment();
+					residentPreview.setArguments(residentArgs);
+					FragmentManager fragmentManager = getSupportFragmentManager();
+					FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+					fragmentTransaction.add(R.id.recent_resident_1, residentPreview);
+					fragmentTransaction.commit();
+					
+					recent1.setVisibility(View.VISIBLE);
+				}
+				break;
+				case 1: {
+					ResidentFragment residentPreview = new ResidentFragment();
+					residentPreview.setArguments(residentArgs);
+					FragmentManager fragmentManager = getSupportFragmentManager();
+					FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+					fragmentTransaction.add(R.id.recent_resident_2, residentPreview);
+					fragmentTransaction.commit();
+					
+					recent2.setVisibility(View.VISIBLE);
+				}
+				break;
+				case 2: {
+					ResidentFragment residentPreview = new ResidentFragment();
+					residentPreview.setArguments(residentArgs);
+					FragmentManager fragmentManager = getSupportFragmentManager();
+					FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+					fragmentTransaction.add(R.id.recent_resident_3, residentPreview);
+					fragmentTransaction.commit();
+					
+					recent3.setVisibility(View.VISIBLE);
+				}
+				break;
+				case 3: {
+					ResidentFragment residentPreview = new ResidentFragment();
+					residentPreview.setArguments(residentArgs);
+					FragmentManager fragmentManager = getSupportFragmentManager();
+					FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+					fragmentTransaction.add(R.id.recent_resident_4, residentPreview);
+					fragmentTransaction.commit();
+					
+					recent4.setVisibility(View.VISIBLE);
+					
+				}
+				break;
+				case 4: {
+					ResidentFragment residentPreview = new ResidentFragment();
+					residentPreview.setArguments(residentArgs);
+					FragmentManager fragmentManager = getSupportFragmentManager();
+					FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+					fragmentTransaction.add(R.id.recent_resident_5, residentPreview);
+					fragmentTransaction.commit();
+					
+					recent5.setVisibility(View.VISIBLE);
+				}
+				break;
+				default: break;
+			}
+		}
 	}
 
 	/**
