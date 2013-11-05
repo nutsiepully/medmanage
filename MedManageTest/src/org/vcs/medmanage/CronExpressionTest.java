cc6110270b3ba8f8227f6c72f7bb1a86fb4dbf8c
Pulkit Bhuwalka <pulkit.bosco05@gmail.com>
2013-11-04 23:32:58 -0500

    Got quartz triggers firing. Woot!

--- MedManageTest/src/org/vcs/medmanage/CronExpressionTest.java
+++ MedManageTest/src/org/vcs/medmanage/CronExpressionTest.java
@@ -2,13 +2,43 @@ package org.vcs.medmanage;
 
 import junit.framework.TestCase;
 
+import org.quartz.CronScheduleBuilder;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.TriggerUtils;
+import org.quartz.impl.calendar.BaseCalendar;
+import org.quartz.spi.OperableTrigger;
+
 import java.util.Date;
+import java.util.List;
 
 public class CronExpressionTest extends TestCase {
 
     public void testGetNextInvalidTimeAfter() throws Exception {
-        CronExpression cronExpression = new CronExpression("* 30,00 11,16 * * * *");
-        Date date = new Date(); date.setHours(10);
-        cronExpression.getNextValidTimeAfter(date);
+        CronExpression cronExpression = new CronExpression("0 30 16 * * ? *");
+        Date today = new Date(2013, 12, 22, 0, 0, 0);
+        Date medicineTime = cronExpression.getNextValidTimeAfter(today);
+
+        assertEquals(new Date(2013, 10, 22, 16, 30, 0), medicineTime);
+    }
+
+    public void testCronFireableTimes() throws Exception
+    {
+       OperableTrigger trigger = (OperableTrigger) TriggerBuilder
+                .newTrigger()
+                .withIdentity("trigger1", "group1")
+                .withSchedule(
+                        CronScheduleBuilder.cronSchedule("0/5 * * * * ?"))
+                .build();
+
+        Date startDate = new Date(); Date endDate = new Date(startDate.getTime() + 1000000);
+        List<Date> dateList = TriggerUtils.computeFireTimesBetween(trigger, new BaseCalendar(), startDate, endDate);
+
+        System.out.println("******Times**********");
+        for(Date date : dateList) {
+            System.out.println(date.toString());
+        }
+        System.out.println("*********************");
     }
 }
