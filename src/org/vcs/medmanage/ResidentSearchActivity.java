985c3e15f7b57e5deb1c82a769460509a8cbe933
Pulkit Bhuwalka <pulkit.bosco05@gmail.com>
2013-11-08 20:29:57 -0500

    Changing background style. Integrating kurts fragments.

--- src/org/vcs/medmanage/ResidentSearchActivity.java
+++ src/org/vcs/medmanage/ResidentSearchActivity.java
@@ -1,40 +1,36 @@
 package org.vcs.medmanage;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-import com.j256.ormlite.dao.DaoManager;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import db.DatabaseHelper;
 import entities.Resident;
-import entities.ResidentUtils;
 
-public class ResidentSearchActivity extends Activity {
+public class ResidentSearchActivity extends FragmentActivity {
 
     private List<String> corridorsList = Arrays.asList(new String[]{"Corridor 1", "Corridor 2", "Corridor 3"});
     private List<String> residentStatusList = Arrays.asList(new String[]{"Red", "Yellow", "Green"});
     private List<String> alphabeticRangeList = Arrays.asList(new String[]{"A-C", "D-G", "H-L", "M-Q", "R-T", "U-Z"});
 
-    private List<Resident> residentList = new ArrayList<Resident>();
-
     private ArrayAdapter<String> corridorsAdapter;
     private ArrayAdapter<String> residentStatusesAdapter;
     private ArrayAdapter<String> alphabeticRangeAdapter;
 
-    private ArrayAdapter<Resident> residentAdapter;
-
     private ResidentService residentService;
+    private LinearLayout residentsLayout;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,12 +44,9 @@ public class ResidentSearchActivity extends Activity {
         residentStatusesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, residentStatusList);
         alphabeticRangeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, alphabeticRangeList);
 
-        residentAdapter = new ArrayAdapter<Resident>(this, R.layout.list_item, R.id.listTextView, residentList);
+        residentsLayout = (LinearLayout)findViewById(R.id.residentSearchLinearLayout);
 
         residentService = new ResidentService(this);
-
-        final ListView residentListView = (ListView) findViewById(R.id.residentListView);
-        residentListView.setAdapter(residentAdapter);
 
         final ListView searchOptionsListView = (ListView) findViewById(R.id.searchOptionsListView);
         searchOptionsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -90,8 +83,23 @@ public class ResidentSearchActivity extends Activity {
                 break;
         }
 
-        residentAdapter.clear();
-        residentAdapter.addAll(searchedResidents);
+        residentsLayout.removeAllViews();
+        addResidentsToView(searchedResidents);
+    }
+
+    public void addResidentsToView(List<Resident> residents){
+        for(int i = 0; i < residents.size(); i++){
+            Bundle residentArgs = new Bundle();
+            residentArgs.putString("ResidentName", residents.get(i).getName());
+            residentArgs.putInt("RoomNumber", residents.get(i).getRoomNumber());
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            ResidentFragment recentFragment = new ResidentFragment();
+            recentFragment.setArguments(residentArgs);
+            fragmentTransaction.add(residentsLayout.getId(), recentFragment);
+            fragmentTransaction.commit();
+        }
     }
 
     public void selectSearchType(View view) {
