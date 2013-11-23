0b99f5d4473fb79b3141eb9e5a9510ad763efeb7
Pulkit Bhuwalka <pulkit.bosco05@gmail.com>
2013-11-22 23:59:47 -0500

    Adding SMS functionality. Setting alert service to 5 mins. SMS doesnt quite work yet.

--- src/org/vcs/medmanage/PatientAlertBackgroundService.java
+++ src/org/vcs/medmanage/PatientAlertBackgroundService.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Parcel;
 import android.support.v4.app.NotificationCompat;
+import android.telephony.SmsManager;
 import android.util.Log;
 
 import java.util.Date;
@@ -40,6 +41,10 @@ public class PatientAlertBackgroundService extends IntentService {
                     getBaseContext(), 0, new Intent(this, LandingPage.class), Intent.FLAG_ACTIVITY_NEW_TASK);
             notification.setLatestEventInfo(this, "Medicine Alert", alert, pendingIntent);
             notificationManager.notify(1, notification);
+
+            PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, LandingPage.class), 0);
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage("14794306665", "14126239623", alert, pi, null);
         }
     }
 
